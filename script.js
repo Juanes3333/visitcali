453706f2316c3d3b3bd251f4caeb5eec1eda401a
Laura Sofia Zapata Montealegre <laurasofiazapatamontealegre@MacBookPro.lan>
2026-05-23 00:14:28 -0500

    cualquiercosa

--- script.js
+++ script.js
@@ -15,3 +15,40 @@ const updateHeaderState = () => {
 
 updateHeaderState();
 window.addEventListener("scroll", updateHeaderState, { passive: true });
+
+const cardVideos = document.querySelectorAll(".feature-card__media");
+
+const playCardVideo = (video) => {
+  if (!video) {
+    return;
+  }
+
+  const playPromise = video.play();
+  if (playPromise && typeof playPromise.catch === "function") {
+    playPromise.catch(() => {});
+  }
+};
+
+const stopCardVideo = (video) => {
+  if (!video) {
+    return;
+  }
+
+  video.pause();
+};
+
+cardVideos.forEach((video) => {
+  const card = video.closest(".feature-card");
+  if (!card) {
+    return;
+  }
+
+  video.addEventListener("loadeddata", () => {
+    video.pause();
+  });
+
+  card.addEventListener("mouseenter", () => playCardVideo(video));
+  card.addEventListener("mouseleave", () => stopCardVideo(video));
+  card.addEventListener("focusin", () => playCardVideo(video));
+  card.addEventListener("focusout", () => stopCardVideo(video));
+});
